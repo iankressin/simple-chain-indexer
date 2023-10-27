2e3e3a729330804d43bf9dab14fec6706be3694b
Ian Guimaraes <ianguimaraes31@gmail.com>
2023-10-27 18:03:15 -0300

    feat: group transaction by block offset

--- src/modules/Analyzer.ts
+++ src/modules/Analyzer.ts
@@ -2,6 +2,7 @@ import { AppDataSource } from '../data-source';
 import { Transaction } from '../entity/Transaction';
 import { Account } from '../entity/Account';
 import { DataSource } from 'typeorm';
+import { group } from 'console';
 
 /**
 * The main goal of this analysis is to find out if are there users doing a sequence of transactions frequently
@@ -34,11 +35,45 @@ export class Analyzer {
             .createQueryBuilder('transaction')
             .select()
             .where('transaction.from = :from', { from: address })
-            .getMany() 
+            .orderBy('transaction.block')
+            .getMany()
+        
 
         console.log('Transactions: ', address, transactions.length)
     }
 
+    // Transactions separated in groups where the block number of a transaction needs to be within 10 blocks of one of other groups
+    // If not, create a new group
+    private groupTransactionsByBlockOffset(transactions: Transaction[]) {
+        const defaultBlockOffset = 40 // ~ 10 minutes
+
+        const groups: {
+            startBlock: number
+            endBlock: number
+            transactions: Transaction[]
+        }[] = []
+
+        transactions.map(transaction => {
+            const group = groups.find(group =>
+                this.isInBetweenBlocks(transaction.block, group.startBlock, group.endBlock)
+            )
+
+            if (group)
+                group.transactions.push(transaction)
+            else
+                groups.push({
+                    startBlock: transaction.block,
+                    endBlock: transaction.block + defaultBlockOffset,
+                    transactions: [transaction],
+                })
+        })
+
+        console.log(groups)
+    }
+
+    private isInBetweenBlocks(block: number, start: number, end: number) {
+        return block >= start && block < end
+    }
 }
 
 (async () => {
